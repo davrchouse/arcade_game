3a04f766ca30dbac31b59bb7ad93435214abb2c8
drch <davrchouse@gmail.com>
2016-06-30 14:35:13 -0700

    add initial enemies to go either direction and on specific row

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var allEnemies = [];
+
+var Enemy = function(name,row,dir) {
+    this.name = name;
+    this.dir = dir;
+    if (dir < 0) {
+        this.x = 555;
+        this.sprite = 'images/enemy-bug2.png'; // ADDED ALTERNATE DIRECTION FOR BUG
+    } else {
+        this.x = 0;
+        this.sprite = 'images/enemy-bug.png';
+    }
+    this.y = 60 + (row * 83);
+    allEnemies.push(this);
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    // ADDED ALTERNATE DIRECTION FOR BUG
+    if (this.dir < 0) {
+        if (this.x > 0) {
+            this.x-=1;
+        } else {
+            this.x = 555;
+            }
+    } else {
+        if (this.x < 505) {
+            this.x++;
+        } else {
+            this.x = 0;
+        }
+    }
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -25,10 +51,33 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var boris = new Enemy("Boris",0,-1);
+var natasha = new Enemy("Natasha",3,1);
+
+var player = new Player();
+
+console.log(allEnemies);
 
 
 
