26ed889237fa9324bad55904e7f8df22324e3072
drch <davrchouse@gmail.com>
2016-06-30 22:19:22 -0700

    add collisions the player and enemies

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
-// Enemies our player must avoid
+// Udacity: Enemies our player must avoid
+// drch: add array for all enemies
 var allEnemies = [];
 
-var Enemy = function(name,row,dir,speed) {
+var Enemy = function(name,row,speed) {
+    // Udacity: Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.name = name;
-    this.dir = dir;
     this.speed = speed;
-    if (dir < 0) {
+    if (speed < 0) {
         this.x = 555;
-        this.sprite = 'images/enemy-bug2.png'; // ADDED ALTERNATE DIRECTION FOR BUG
+        this.sprite = 'images/enemy-bug2.png'; // drch: ADDED ALTERNATE DIRECTION FOR BUG
     } else {
         this.x = 0;
         this.sprite = 'images/enemy-bug.png';
     }
     this.y = 60 + (row * 83);
     allEnemies.push(this);
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
 };
 
-// Update the enemy's position, required method for game
+// Udacity: Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // ADDED ALTERNATE DIRECTION FOR BUG
-    if (this.dir < 0) {
+    // drch ADDED ALTERNATE DIRECTION FOR BUG
+    // Udacity: You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    if (this.speed < 0) {
         if (this.x > -100) {
-            this.x-=this.speed*dt;
+            this.x+=this.speed*dt;
         } else {
             this.x = 555;
             }
@@ -38,17 +41,19 @@ Enemy.prototype.update = function(dt) {
             this.x = 0;
         }
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
+// Udacity: Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    // ctx.fillStyle = "rgba(100,100,100,0.5)"; // uncomment to highlight total png for each enemy
+    // ctx.fillRect(this.x,this.y,101,171);
+    // ctx.fillStyle = "rgba(255,255,0,0.75)"; // uncomment to highlight collision zone on each enemy
+    // ctx.fillRect(this.x,this.y+78,101,65);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// drch: Player function for moving player from event listener for input keys (below)
+// Udacity: Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
@@ -58,9 +63,11 @@ var Player = function () {
     this.movementX = 0;
     this.movementY = 0;
     this.sprite = 'images/char-boy.png';
+    // var test = this.sprite.width; // how to find width of sprite for the enemy to bump into it?
+    // console.log(test); // ??
 };
 
-// handleInput function of each player that translates input key into
+// drch: handleInput function of each player that translates input key into
 // an x or y movement to add in the update function
 Player.prototype.handleInput = function(inputKey) {
     if (inputKey === "left") {
@@ -75,45 +82,88 @@ Player.prototype.handleInput = function(inputKey) {
      else if (inputKey === "up") {
         this.movementY = -30;
     }
-};
+    setTimeout(function(){
+        console.log("player", player.x,player.y)},200);
+    };
+
+var collision = 0;
 
 Player.prototype.update = function() {
-    if (this.y > 435) {
-        this.y = 435;
-    } else if (this.x < -10) {
-        this.x = -10;
-    } else if (this.y < -10) {
-        this.y = -10;
-    } else if (this.x > 412) {
-        this.x = 412;
+    var player = this;
+    // set boundaries in png for the player and each enemy
+    var playerLeft = player.x+20;
+        var playerRight = player.x+79;
+        var playerTop = player.y+65;
+        var playerBottom = player.y+139;
+    allEnemies.forEach(function(enemy) {
+        var enemyLeft = enemy.x;
+        var enemyRight = enemy.x+101;
+        var enemyTop = enemy.y+78;
+        var enemyBottom = enemy.y+139;
+        // label as a collision if the player touches any of the enemies
+        if (
+            (
+                ((playerTop < enemyBottom) && (playerTop > enemyTop))
+                ||
+                ((playerBottom > enemyTop) && (playerBottom < enemyBottom))
+            )
+            &&
+            (
+                ((playerLeft > enemyLeft) && (playerLeft < enemyRight))
+                ||
+                ((playerRight > enemyLeft) && (playerRight < enemyRight))
+            )
+        ) {
+            collision = 1;
+        } else {
+            collision = 0;
+    };
+    // if a collision occurs, reset the player to the starting point and reset the collision counter to 0;
+    if (collision === 1) {
+        player.x = (405 / 2);
+        player.y = 405;
+        collision = 0;
     } else {
-    this.x += this.movementX;
-    this.y += this.movementY;
-    this.movementX = 0;
-    this.movementY = 0;
-    }
+            if (player.y > 435) {
+                player.y = 435;
+            } else if (player.x < -10) {
+                player.x = -10;
+            } else if (player.y < -10) {
+                player.y = -10;
+            } else if (player.x > 412) {
+                player.x = 412;
+            } else {
+                player.x += player.movementX;
+                player.y += player.movementY;
+                player.movementX = 0;
+                player.movementY = 0;
+            }
+        }
+    });
 };
 
-// Draw the enemy on the screen, required method for game
 Player.prototype.render = function() {
+    // ctx.fillStyle = "rgba(255,255,255,0.5)"; // uncomment to highlight total png for player
+    // ctx.fillRect(this.x,this.y,101,171);
+    // ctx.fillStyle = "rgba(255,255,0,0.75)"; // uncomment to highlight collision zone for player
+    // ctx.fillRect(this.x+20,this.y+65,63,74);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-// Now instantiate your objects.
+
+// drch: make multiple enemies with row, speed and direction info
+// Udacity: Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var boris = new Enemy("Boris",0,-1,200);
-var natasha = new Enemy("Natasha",3,1,50);
-var igor = new Enemy("Igor",2,-1,150);
-var dmitri = new Enemy("Dmitri",1,1,100);
+var boris = new Enemy("Boris",0,50);
+var dmitri = new Enemy("Dmitri",1,-100);
+var natasha = new Enemy("Natasha",3,-50);
+var igor = new Enemy("Igor",2,150);
 
+// drch: add player instance
 var player = new Player();
-
-console.log(allEnemies);
-console.log(player);
-
 
 
 // This listens for key presses and sends the keys to your
